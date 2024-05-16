1df7cf7dbbb9e6610fc2ea779852db9259a36494
HieuGit <meiiforjob@gmail.com>
2024-05-16 17:11:16 +0700

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@nestjs/common";
+@Injectable({}) // this is "Dependency Injection"
+export class AuthService {
+    register() {
+
+    }
+    login() {
+        
+    }
+}
